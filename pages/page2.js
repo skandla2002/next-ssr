0298543c3f05a8f83ca522bedbb39c7954306461
JONGMEONG PAK <skandla2002@gmail.com>
2020-07-24 19:45:20 +0900

    내가 기억 하는 부분

--- pages/page2.js
+++ pages/page2.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import Router from 'next/router';
+import callApi from '../src/api';
+
 
 export default class page2 extends Component {
+
+    static async getInintialProps({ query }) {
+        const text = query;
+        const data = await callApi();
+        return { text, data};
+    }
 
     onClick = () => {
         Router.push('/page1');
     }
 
     render() {
+        const { text, data } = this.props;
+        
         return (
             <div>
                 <button onClick={this.onClick}>page 1으로 </button>
                 <p>page 2</p>
+                <p>{`text: ${text}`}</p>
+                <p>{`data is ${data}`}</p>
             </div>
         )
     }
