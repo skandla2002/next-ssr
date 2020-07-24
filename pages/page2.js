a0c656e546c63b3e0cafa4a7c762b3be5c58fdfc
JONGMEONG PAK <skandla2002@gmail.com>
2020-07-24 20:06:12 +0900

    next 초급까지 커밋

--- pages/page2.js
+++ pages/page2.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Router from 'next/router';
 import { callApi } from '../src/api';
+import Error from './_error';
 
 
 export default class page2 extends Component {
 
-    static async getInintialProps({ query }) {
-        const text = query;
+    static async getInintialProps({ query, req }) {
+        // throw new Error('exception in getInitialProps');
+        const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+        const text = query.text || 'none';
         const data = await callApi();
-        return { text, data};
+        return { text, data, userAgent};
     }
 
     onClick = () => {
@@ -16,14 +19,15 @@ export default class page2 extends Component {
     }
 
     render() {
-        const { text, data } = this.props;
+        const { text, data, userAgent } = this.props;
         
         return (
             <div>
-                <button onClick={this.onClick}>page 1으로 </button>
+                {/* <button onClick={this.onClick}>page 1으로 </button> */}
                 <p>page 2</p>
                 <p>{`text: ${text}`}</p>
                 <p>{`data is ${data}`}</p>
+                <p>{`userAgent is ${userAgent}`}</p>
             </div>
         )
     }
